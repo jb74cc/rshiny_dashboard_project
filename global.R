cec31a5158eaaad9ee2cb8c69ed78b3be8003637
Jerry Balloch <jerry.balloch@gmail.com>
2022-04-26 21:16:49 +0100

    colour palettes added to global file

--- global.R
+++ global.R
@@ -20,3 +20,25 @@ sco_hb <- st_read('raw_data/SG_NHS_HealthBoards_2019/SG_NHS_HealthBoards_2019.sh
 
 sco_hb_simplified <-  st_simplify(sco_hb, preserveTopology = TRUE,
                                   dTolerance = 1000)
+
+# colour palette
+nhs_colours <-  c("Ayrshire and Arran" = "#45214A", 
+                  "Borders" = "#323050", 
+                  "Dumfries and Galloway" = "#21445B", 
+                  "Forth Valley" = "#1A6566", 
+                  "Grampian" = "#5D8A66",
+                  "Highland" = "#033B3D", 
+                  "Lothian" = "#0D4A3A", 
+                  "Orkney" = "#2D5731", 
+                  "Shetland" = "#526126", 
+                  "Western Isles" = "#7B6823",
+                  "Fife" = "#0B2559", 
+                  "Tayside" = "#183B59", 
+                  "Greater Glasgow and Clyde" = "#2A5159", 
+                  "Lanarkshire" = "#327355")
+
+
+# alternative colour palette
+nhs_colours_2 <-  c("#406771", "#73BFA3", "#F2B263", "#F2594B", "#F23030",
+                  "#34401A", "#F2A03D", "#F27F1B", "#590202", "#0B2559",
+                  "#2A5159", "#1EA4D9", "#9B4393", "#433685")
